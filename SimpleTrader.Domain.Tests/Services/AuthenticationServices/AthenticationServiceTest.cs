c9e49e31c13df46ffa31b78a6ffa5b444ed06f19
Ric-001 <ricalcaide@yahoo.es>
2026-04-09 16:03:23 +0200

    Añade proyecto de tests unitarios SimpleTrader.Domain.Tests
    Se incorpora el proyecto SimpleTrader.Domain.Tests a la solución, configurado para .NET 10.0 y con dependencias para NUnit y cobertura de código. Se agrega la clase base AthenticationServiceTest como punto de partida para pruebas de servicios de autenticación.

---
+++ SimpleTrader.Domain.Tests/Services/AuthenticationServices/AthenticationServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.Domain.Tests.Services.AuthenticationServices
+{
+    internal class AthenticationServiceTest
+    {
+
+    }
+}
