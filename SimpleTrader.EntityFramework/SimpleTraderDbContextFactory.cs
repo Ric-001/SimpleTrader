37aa97cd87c5944d52f45c86da17f9dbefa012e8
Ricardo Casa <ralcaide@multipetroleos.com>
2026-04-14 18:37:45 +0200

    Añade iconos y soporte de configuración en español
    - Añadidos iconos PNG para "Cartera", "Compra" y "Venta" en la barra de navegación y traducción de textos al español.
    - Actualizado el proyecto para incluir y copiar los nuevos iconos.
    - Añadido `appsettings.json` con cadena de conexión y configuración de API.
    - Modificada `SimpleTraderDbContextFactory` para usar configuración externa y soportar inyección de dependencias.

--- SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
+++ SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
@@ -1,17 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SimpleTrader.EntityFramework
 {
+
     public class SimpleTraderDbContextFactory : IDesignTimeDbContextFactory<SimpleTraderDbContext>
     {
+        private readonly IConfiguration? _configuration;
+
+        public SimpleTraderDbContextFactory(IConfiguration? configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        // Usado por EF Core en tiempo de diseño (migraciones), sin DI
+        public SimpleTraderDbContextFactory() { }
+
         public SimpleTraderDbContext CreateDbContext(string[]? args = null)
         {
+            var connectionString = _configuration?.GetConnectionString("Default")
+                ?? "Server=(localdb)\\MSSQLLocalDB;Database=SimpleTraderDB;Trusted_Connection=True;";
+
             var options = new DbContextOptionsBuilder<SimpleTraderDbContext>();
-            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SimpleTraderDB;Trusted_Connection=True;");
+            options.UseSqlServer(connectionString);
 
             return new SimpleTraderDbContext(options.Options);
         }
